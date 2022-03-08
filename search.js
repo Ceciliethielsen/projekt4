8392e33ea6fbc668fe887fc225a232e800249815
Ceciliethielsen <cecilie.mft@hotmail.com>
2022-03-08 20:50:37 +0100

    Initial commit

---
+++ search.js
@@ -0,0 +1,50 @@
+
+$(".fa-search").click(function(){
+    $(".icon").toggleClass("active");
+    $("input[type=text]").toggleClass("active");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Data som kan blive søgt efter
+//const resultater = [
+  //  {name: 'multimediedesigner'},
+    //{name: 'Personligt projekt'},
+    //{name: 'studiejob'},
+
+//]
+
+//tilføjer event-listener, som venter på, at noget skal ske (fx klik, input mv)
+//bruger queryselector til at hente knap ved class-navn og sætter det i en const
+//whateverElement.addEventListener("click", (e) => {
+    //const searchInput = document.querySelector(".input")
+
+    //let value = e.target.value
+
+    //if (value && value.trim().length > 0){
+        //value = value.trim(). toLowerCase()
+    //} else {}
+
+//})
+
+
