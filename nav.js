dd3b0bb154c951ae7292decd6dad074474d136ae
Ceciliethielsen <cecilie.mft@hotmail.com>
2022-03-09 23:59:49 +0100

    ryddet op + burgermenu

--- nav.js
+++ nav.js
@@ -1,61 +1,8 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".hamburger");
- 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
- 
-document.querySelectorAll(".nav-link").forEach(n => n.
-    addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }))
-    
-    alert("Test for at se om js virker!");
-    
+ alert("Test for at se om js virker!");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Data som kan blive søgt efter
-//const resultater = [
-  //  {name: 'multimediedesigner'},
-    //{name: 'Personligt projekt'},
-    //{name: 'studiejob'},
-
-//]
-
-//tilføjer event-listener, som venter på, at noget skal ske (fx klik, input mv)
-//bruger queryselector til at hente knap ved class-navn og sætter det i en const
-//whateverElement.addEventListener("click", (e) => {
-    //const searchInput = document.querySelector(".input")
-
-    //let value = e.target.value
-
-    //if (value && value.trim().length > 0){
-        //value = value.trim(). toLowerCase()
-    //} else {}
-
-//})
-
-
+    var burgerMenu = document.getElementById('burgermenu');
+    var overlay = document.getElementById('menu');
+    burgerMenu.addEventListener('click',function(){
+      this.classList.toggle("close");
+      overlay.classList.toggle("overlay");
+    });
