e9c00a70d1f9ee9492b9087e03c258daa3db2701
Ceciliethielsen <cecilie.mft@hotmail.com>
2022-03-14 22:35:06 +0100

    Validator
    rettelser efter validation

--- likeknap.js
+++ likeknap.js
@@ -1,8 +1,10 @@
 //Mateusz' JS start
 //alert("JS VIRKER!")
-//console.log("main.js is running...");
+console.log("main.js is running...");
+var numberoflikes = 0
+ 
 function myFunction() {
-    numberoflikes = numberoflikes + 1;
+    numberoflikes = numberoflikes + 1
     document.getElementById("likeamount").innerHTML = numberoflikes;
 }
-//Mateusz' JS slut
+//Mateusz JS slut
